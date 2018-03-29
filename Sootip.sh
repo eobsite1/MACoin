3b4b0b464633ff8cb4bd0f7a092639651ed269ab
eobsite1 <30572208+eobsite1@users.noreply.github.com>
2018-03-29 21:40:53 +0100

    Create Sootip.sh

---
+++ Sootip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo "gibe unique waltet id nao"
+read $waltet
+clear
+echo "gibe peshwerd 2 conz"
+read $pesh
+export PESH=$pesh
+clear
+echo "yuh getz 1 conz freh"
+read fukyah
+export SPESHUL=$RANDOM$RANDOM$RANDOM
+touch ~/.$SPESHUL
+echo "1 conz fur yuh" >> .$SPESHUL
+echo "yur speshul iz 2 stahp rubberee"
+echo "speshul: " $SPESHUL
+echo "doh nat furgutz teh speshul if yuh doh nat wunt 2 coolyde teh speshul"
+read fukyah
+echo "sootip iz duun"
+read fukyah
+exit 0

55c3be5271efd657bc6d865d1a9cc6d3da5f9177
eobsite1 <30572208+eobsite1@users.noreply.github.com>
2018-03-30 22:37:39 +0000

    Update perggg.sh
    ee deed sum sheet

--- perggg.sh
+++ perggg.sh
@@ -1,10 +1,25 @@
+clear
+echo "wus yo mf pesh mannn"
+while:
+do
+read peshchuk
+case peshchuk in
+$PESH)
+echo "ayyyyy yuh gets id rite"
+break
+;;
+*)
+echo "git adda hur wit dat bew sheet"
+;;
+esac
+done
 echo "yuh wan git red offf yo cunz? [Yuh/Naw]"
 read answa
 while:
 do
 case $answa in
 Yuh)
-rm .$whawuzdatvar?
+rm .$SPESHUL
 echo "OOF yuu brok nao ripp"
 read awwfuk
 break
@@ -17,6 +32,7 @@ break
 *)
 echo "wot teh fuk ching chong ass werdz u usin, answa teh fukin question"
 ;;
+esac
 done
 echo "dis script wuz fukin dum aniwae"
 exit 0

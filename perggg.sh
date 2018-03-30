06ce4f2186f0b2ff726a98e71c708ce41a84ee56
eobsite1 <30572208+eobsite1@users.noreply.github.com>
2018-03-30 22:32:57 +0000

    Create perggg.sh
    i gon git dat ver nao

---
+++ perggg.sh
@@ -0,0 +1,22 @@
+echo "yuh wan git red offf yo cunz? [Yuh/Naw]"
+read answa
+while:
+do
+case $answa in
+Yuh)
+rm .$whawuzdatvar?
+echo "OOF yuu brok nao ripp"
+read awwfuk
+break
+;;
+Naw)
+echo "Fuk yuu ruhn meh fo den?"
+read awwyeh
+break
+;;
+*)
+echo "wot teh fuk ching chong ass werdz u usin, answa teh fukin question"
+;;
+done
+echo "dis script wuz fukin dum aniwae"
+exit 0

d83811e8623963636ee03a975c1f616029d7363a
eobsite <eobsite>
2018-03-31 19:15:36 +0000

    sum cood dat iz nut dunn

--- Sootip.sh
+++ Sootip.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 clear
 echo "gibe unique waltet id nao"
-read $waltet
+read waltet
 clear
 echo "gibe peshwerd 2 conz"
-read $pesh
+read pesh
 export PESH=$pesh
 clear
 echo "yuh getz 1 conz freh"
 read fukyah
 export SPESHUL=$RANDOM$RANDOM$RANDOM
-touch ~/.$SPESHUL
-echo "1 conz fur yuh" >> .$SPESHUL
+touch ~/.$waltet
+echo "1 conz fur yuh mestor "$waltet  >> ~/.$waltet
+echo "dis conz iz fo dis speshul: "$SPESHUL >> ~/.$waltet
+echo "dis conz is fo dis pesh: "$pesh >> ~/.$waltet
 echo "yur speshul iz 2 stahp rubberee"
 echo "speshul: " $SPESHUL
 echo "doh nat furgutz teh speshul if yuh doh nat wunt 2 coolyde teh speshul"
